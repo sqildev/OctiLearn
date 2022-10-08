765b0824fa1ed8f1dd0fc070dd7ba0a76a599b94
Krish Kapoor <kapoorkrish26@gmail.com>
2022-10-08 21:02:37 +0000

    Update _app.js and about.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,5 @@
-import React from "react";
-import Head from "next/head";
+ import React from "react";
+//import Head from "next/head";
 import "../styles/globals.css";
 
 import store from "../state/store";

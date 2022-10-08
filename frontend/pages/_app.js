39b9bc4627d8afa1cc61520c9ddab73e5031aa4f
validName <octilearnteam@gmail.com>
2022-10-08 05:05:16 +0000

    hi

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -9,15 +9,15 @@ const App = ({ Component, pageProps }) => {
   const getLayout = Component.getLayout || ((page) => page);
   return (
     <Provider store={store}>
-      <Head>
+      {/*<Head>
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
           integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
           referrerpolicy="no-referrer"
         />
-      </Head>
+      </Head>*/}
       {getLayout(<Component {...pageProps} />)}
     </Provider>
   );
